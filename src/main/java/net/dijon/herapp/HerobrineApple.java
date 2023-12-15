eff25748a72bb2dccb6e2f10c602f392ee92fea4
Dij0n <dijonmustrdlive@gmail.com>
2023-12-15 18:02:57 +0300

    Initial commit

---
+++ src/main/java/net/dijon/herapp/HerobrineApple.java
@@ -0,0 +1,16 @@
+package net.dijon.herapp;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HerobrineApple implements ModInitializer {
+	public static final String MOD_ID = "herobrineapple";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/dijon/herapp/HerobrineAppleClient.java
@@ -0,0 +1,10 @@
+package net.dijon.herapp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HerobrineAppleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
